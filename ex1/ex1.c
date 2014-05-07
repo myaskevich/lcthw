42fb4649e0268a7c62199a6ed8bb76894c9966db
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2014-05-07 22:44:42 +0300

    Replaced abusing content with silly text for ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    puts("Hello, World.\nMy name is Zed, and I'm going to be teaching you C language.");
-    puts("I know you're experienced in Python, and a little of C++\n\r");
-    puts("You're lucky, this will help you in future. Bye!");
+    puts("Hello, World.\nAdded few more lines.");
+    puts("Added some text here too\r\n");
+    puts("Last line containing silly text here.");
 
     return 0;
 }
