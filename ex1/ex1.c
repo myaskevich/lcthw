8f150060187a554b5bcc5df8e0d336f83da1540a
Maxim Yaskevich <yaskevichmaxim@gmail.com>
2014-05-07 22:33:25 +0300

    Added ex1

---
+++ ex1/ex1.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    puts("Hello, World.\nMy name is Zed, and I'm going to be teaching you C language.");
+    puts("I know you're experienced in Python, and a little of C++\n\r");
+    puts("You're lucky, this will help you in future. Bye!");
+
+    return 0;
+}
